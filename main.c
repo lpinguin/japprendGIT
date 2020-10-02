afca0c688bc5e24acaa269e0f07724029917b57b
Adrien <adrien@phoenix>
2020-10-02 12:19:59 +0200

    creation du code source principale nomm  main.c.

---
+++ main.c
@@ -0,0 +1,59 @@
+/*
+    C SOURCE CODE      
+	Project Name:
+	Purpose of source code:
+
+---------------------------------------------------------------------
+COMPILER NEDEED
+	HARDWARE TARGET: 
+	DIALECT: 	-std={gnu89, gnu90, gnu99, gnu11,}
+
+	-pedantic Pendantic option:
+
+	-pedantic-error Pedantic error option 
+
+	NON-STANDART LIBRARY -if used- : 
+
+---------------------------------------------------------------------
+
+SOURCE FILE NEDEED
+	Non standart library -if use-:
+/*
+PURPOSE:
+
+FUNCTIONS USED:
+
+VARIABLES USED:
+
+CONDICTIONS 
+
+Note: .
+
+TIPS:
+	Source file Must end with a newline charactere.
+////////////////////////////////////////////////////////////////////////
+
+
+*/
+
+/*INCLUDE*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+
+/*PROTOTYPE*/
+
+void nomFonction(parametres)
+
+
+/*MAIN*/
+
+int main(int argc, char *argv[])
+	{
+
+
+		return 0 ;
+	}
+/*-------------------------------------------------------------------*/
