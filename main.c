44c9b2a99851cdc1b563aada85be47fe8f1299d8
lpinguin <54332844+lpinguin@users.noreply.github.com>
2020-10-04 14:23:16 +0200

    Add file via upload
    Premiere version mise en ligne via l'interface web.

---
+++ main.c
@@ -0,0 +1,60 @@
+/*
+    C SOURCE CODE      
+	Project Name: J'apprend git. 
+	Purpose of source code: Code source de la branche Master. 
+	Teste d'utilisation de Git avec les editions diamond.
+---------------------------------------------------------------------
+COMPILER NEDEED
+	HARDWARE TARGET: 
+	DIALECT: 	-std={gnu89, gnu90, gnu99, gnu11,}
+
+	-pedantic Pendantic option:
+
+	-pedantic-error Pedantic error option 
+
+	NON-STANDART LIBRARY -if used- : 
+
+---------------------------------------------------------------------
+
+SOURCE FILE NEDEED
+	Non standart library -if use-:
+
+PURPOSE:
+
+FUNCTIONS USED:
+	_printf();
+	_main()
+VARIABLES USED:
+
+CONDICTIONS 
+
+Note: .
+
+TIPS:
+	Source file Must end with a newline charactere.
+////////////////////////////////////////////////////////////////////////
+
+
+*/
+
+/*INCLUDE*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+
+/*PROTOTYPE*/
+
+void nomFonction(parametres)
+
+
+/*MAIN*/
+
+int main(int argc, char *argv[])
+	{
+
+
+		return 0 ;
+	}
+/*-------------------------------------------------------------------*/
